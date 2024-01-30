f40f5df196977d1a9395dede86ad38b8dce60bf7
MahmoodAhmed-SE <Mahmood.Ahmed.SE@outlook.com>
2024-01-30 20:54:22 +0400

    Finalizing the project.

--- js/app.js
+++ js/app.js
@@ -24,10 +24,12 @@
 */
 
 // Navigation 'ul' element, this will be used as a container to the 'li' elements.
-const nav = document.getElementById("navbar__list") 
+const nav = document.getElementById("navbar__list")
 
 // this list contains the availble 'section' elements, that will be used to set the navigation list elements.
 const sections = document.querySelectorAll("section");
+
+
 
 /**
  * End Global Variables
@@ -43,12 +45,14 @@ const getOffset = (element) => {
 // [removeClassName] function will help remove a class name from a given element.
 const removeClassName = (element, className) => {
     element.classList.remove(className);
-} 
+}
 
 // [addClassName] does the opposite of [removeClassName].
 const addClassName = (element, className) => {
     element.classList.add(className);
-} 
+}
+
+const getLinks = () => document.querySelectorAll(".navbar__menu a");
 
 /**
  * End Helper Functions
@@ -72,9 +76,9 @@ const buildNav = () => {
         const itemLink = document.createElement('a');
 
         // The following, will set the class name, hyper reference, and the text content to the 'a' element :
-        itemLink.className = "menu__link "; 
-        itemLink.href = `#${section.id}`; 
-        itemLink.textContent = section.dataset.nav;  
+        itemLink.className = "menu__link ";
+        itemLink.href = `#${section.id}`;
+        itemLink.textContent = section.dataset.nav;
 
         // This appends the li and a elements created to the navigation list fragment [navListsFragment].
         sectionItem.appendChild(itemLink);
@@ -85,54 +89,61 @@ const buildNav = () => {
     This loop is used to ensure that there is navigation list items that exist in the html
     somehow before adding the list elements. 
     */
-    while(nav.firstChild) {
+    while (nav.firstChild) {
         nav.removeChild(nav.firstChild);
     }
 
     // This will repaint and reflow the document to set the navigation list items.
     nav.appendChild(navListsFragment);
-} 
+}
 
-
-// Invoking the building function to set the navigation list.
-buildNav();
 
 
 // Add class 'active' to section when near top of viewport
 
-// [getOffset] will be a window scroll EventListener. It will be 
-const activateSectionInView = () => {
-    sections.forEach((section) => {
+const activeSectionListener = () => {
+    sections.forEach(section => {
         const offset = getOffset(section);
 
         removeClassName(section, "your-active-class");
         if (offset < 150 && offset > -150) {
             addClassName(section, "your-active-class");
         }
-    });     
+    });
+}
+
+
+const activateSectionInView = () => {
+    const navigationLinks = getLinks();
+
+    console.log(navigationLinks[0].innerHTML);
+    navigationLinks.forEach(link => {
+        link.addEventListener("click", (event) => scrollToSectionByLink(event, link.href));
+    });
 }
 
 // For each scroll event there, [activateSectionInView] will be invoked
-window.addEventListener('scroll', activateSectionInView);
+const setSectionInViewListeneres = () => {
+    window.addEventListener('scroll', activateSectionInView);
+}
+
+
 
 
 // Scroll to anchor ID using scrollTO event
 
-// 
-
-const navigationLinks = document.querySelectorAll(".navbar__menu .menu__link");
-
+// [scrollToSectionByLink] function invoked when a link is clicked and smoothly navigates to corresponding section. 
 const scrollToSectionByLink = (event, sectionId) => {
     /* 
     I am preventing default behavior, especially, the addition to the url "#{sectionid}"
     which makes the url a bit unclean with the '#' symbol. 
     */
-    
+
     event.preventDefault();
 
     // This is to extract section ID from link href (href returns absolute url)
     sectionId = sectionId.split('#')[1];
-    
+
     const section = document.getElementById(sectionId);
 
     // scroll to section using the y axis of the top of section given by link 'href'.  
@@ -142,9 +153,18 @@ const scrollToSectionByLink = (event, sectionId) => {
     });
 }
 
-navigationLinks.forEach(link => {
-    link.addEventListener("click", (event) => scrollToSectionByLink(event, link.href));
-});
+// [setLinkEventListeners] function is used to navigate using the link (anchor) to the corresponding section 
+const setLinkEventListeners = () => {
+    const navigationLinks = getLinks();
+
+    navigationLinks.forEach(link => {
+        link.addEventListener("click", event => {
+
+            scrollToSectionByLink(event, link.href);
+        });
+    });
+}
+
 
 /**
  * End Main Functions
@@ -153,9 +173,10 @@ navigationLinks.forEach(link => {
 */
 
 // Build menu 
+buildNav();
 
 // Scroll to section on link click
+setLinkEventListeners();
 
 // Set sections as active
-
-
+activateSectionInView();
